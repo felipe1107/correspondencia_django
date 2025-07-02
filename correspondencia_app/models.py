c7dec2472711a844a1bb3c0e9481fcf6f542432f
Felipe Pinto <tu.email@ejemplo.com>
2025-07-02 11:30:22 -0500

    Publico última versión antes de revisar errores

--- correspondencia_app/models.py
+++ correspondencia_app/models.py
@@ -35,3 +35,18 @@ class CorrespondenciaEntrada(models.Model):
 
     def __str__(self):
         return f"{self.numero_documento} – {self.asunto}"
+
+
+class CorrespondenciaSalida(models.Model):
+    numero_documento = models.CharField(max_length=20)
+    fecha_envio = models.DateField()
+    remitente = models.CharField(max_length=100)
+    destinatario = models.CharField(max_length=100)
+    asunto = models.TextField()
+    departamento_origen = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+    archivo_adjunto = models.FileField(upload_to='salidas/', blank=True, null=True)
+    observaciones = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.numero_documento} - {self.asunto}"
