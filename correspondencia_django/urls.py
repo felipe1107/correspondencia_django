dea767589a1d49d67f9afcbfece3d6e3f593f6ac
Felipe Pinto <tu.email@ejemplo.com>
2025-07-03 10:36:02 -0500

    Publico última versión antes de revisar errores

--- correspondencia_django/urls.py
+++ correspondencia_django/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LogoutView
 from correspondencia_app.views import CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('correspondencia_app.urls', 'correspondencia_app'), namespace='correspondencia_app')),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('', include(('correspondencia_app.urls', 'correspondencia_app'), namespace='correspondencia_app')),
 ]
