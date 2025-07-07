14f1e44af7506b30397314c6c6d4a9c1de31b70a
Felipe Pinto <tu.email@ejemplo.com>
2025-07-07 10:04:12 -0500

    Publico última versión antes de revisar errores

--- correspondencia_django/urls.py
+++ correspondencia_django/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-   path('', lambda request: redirect('login'), name='root'),
+    path('', lambda request: redirect('dashboard'), name='root'),
     path('', include('correspondencia_app.urls')),
     path('admin/', admin.site.urls),
 ]
